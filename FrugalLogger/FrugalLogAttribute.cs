74a27533f8f461a23a6eaaa8d50109dc2f01f02c
Yinglue Zhang <yinglzh@microsoft.com>
2020-07-27 15:58:35 +0800

    pre work

--- FrugalLogger/FrugalLogAttribute.cs
+++ FrugalLogger/FrugalLogAttribute.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
 
 namespace FrugalLogger
 {
-    class FrugalLogAttribute
+    class FrugalLogAttribute : ActionFilterAttribute
     {
     }
 }
