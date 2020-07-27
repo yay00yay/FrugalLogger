2c6ab530c573516c7c1251af12c7ca5962543a68
Yinglue Zhang <yinglzh@microsoft.com>
2020-07-27 15:26:49 +0800

    build proj

---
+++ FrugalLoggerPlayground/Controllers/ToyController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrugalLoggerPlayground.Controllers
+{
+    public class ToyController
+    {
+    }
+}
---
+++ FrugalLoggerPlayground/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FrugalLoggerPlayground.WebApiApplication" Language="C#" %>
