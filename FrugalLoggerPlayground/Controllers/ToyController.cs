a2c55ee235d50f27dc3d506b00f2570607d6421d
Yinglue Zhang <yinglzh@microsoft.com>
2020-07-27 15:50:13 +0800

    toyController

--- FrugalLoggerPlayground/Controllers/ToyController.cs
+++ FrugalLoggerPlayground/Controllers/ToyController.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 
 namespace FrugalLoggerPlayground.Controllers
 {
-    public class ToyController
+    public class ToyController : ApiController
     {
+        [Route("Toy")]
+        [HttpGet]
+        public HttpResponseMessage ToyGet(DateTime date, string str)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response.Content = new StringContent(JsonConvert.SerializeObject("Toy"));
+
+            return response;
+        }
     }
 }
